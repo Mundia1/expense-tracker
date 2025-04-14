dfbf642c4ce49b4fb91813a3adabde863caee978
Mundia1 <waithaka.mundia@gmail.com>
2025-04-14 18:20:50 +0300

    feat:  update index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
 import { useState, useCallback } from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Container from './components/layout/Container/Container';
 import Header from './components/layout/Header/Header';
 import ExpenseForm from './components/features/ExpenseForm/ExpenseForm';
@@ -8,8 +9,6 @@ import ExpenseActions from './components/features/ExpenseActions/ExpenseActions'
 import ExpenseTable from './components/features/ExpenseTable/ExpenseTable';
 import './App.css';
 
-
-// src/functinality
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [sortConfig, setSortConfig] = useState({ field: null, order: 'asc' });
@@ -22,13 +21,29 @@ const App = () => {
   }, []);
 
   return (
-    <Container>
-      <Header />
-      <SearchBar setSearchTerm={setSearchTerm} />
-      <ExpenseForm />
-      <ExpenseActions sortConfig={sortConfig} updateSort={updateSort} />
-      <ExpenseTable searchTerm={searchTerm} sortConfig={sortConfig} />
-    </Container>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Container>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar setSearchTerm={setSearchTerm} />
+                <ExpenseForm />
+                <ExpenseActions sortConfig={sortConfig} updateSort={updateSort} />
+                <ExpenseTable searchTerm={searchTerm} sortConfig={sortConfig} />
+              </>
+            }
+          />
+          {/* You can add other routes here as needed */}
+          {/* Example:
+          <Route path="/expenses" element={<ExpensesPage />} />
+          <Route path="/report" element={<ReportPage />} />
+          */}
+        </Routes>
+      </Container>
+    </Router>
   );
 };
 
