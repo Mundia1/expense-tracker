4da47453ec5448d75d4d785a689a7b1f1dc711fc
Mundia1 <waithaka.mundia@gmail.com>
2025-04-14 16:06:16 +0300

    feat:  updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import SearchBar from './components/features/SearchBar/SearchBar';
 import ExpenseActions from './components/features/ExpenseActions/ExpenseActions';
 import ExpenseTable from './components/features/ExpenseTable/ExpenseTable';
 import './App.css';
-import '../src/styles/global.css';
+
 
 
 const App = () => {
